f05b35defcfe4462aff479da3114836cb6dd7c12
Darsh Mistry <1111darsh@gmail.com>
2018-10-15 16:20:32 +0530

    darsh

---
+++ app/src/main/java/com/escrow/smartexamination/Activity/Registration.java
@@ -0,0 +1,17 @@
+package com.escrow.smartexamination.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.escrow.smartexamination.R;
+
+public class Registration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+
+    }
+}
